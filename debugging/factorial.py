018f0947813dfe97ef4974e442fbf57e6bf56862
neonbloo25 <theluckdemon@gmail.com>
2025-01-13 14:45:22 -0400

    vscode debut

--- debugging/factorial.py
+++ debugging/factorial.py
@@ -2,28 +2,11 @@
 import sys
 
 def factorial(n):
-    if n == 0 or n == 1:
-        return 1
     result = 1
     while n > 1:
         result *= n
-        n -= 1
+        n -= 1  # Decrement n in each iteration
     return result
 
-if len(sys.argv) < 2:
-    print("Usage: python3 script.py <number>")
-    sys.exit(1)
-
-try:
-    n = int(sys.argv[1])
-except ValueError:
-    print("Error: The input must be an integer.")
-    sys.exit(1)
-
-if n < 0:
-    print("Error: The input must be a non-negative integer.")
-    sys.exit(1)
-
-f = factorial(n)
+f = factorial(int(sys.argv[1]))
 print(f)
-
---
+++ debugging/print_arguments.py
@@ -0,0 +1,5 @@
+print_arguments.py  # This is sys.argv[0], the name of the script
+1                   # This is sys.argv[1], the first argument passed
+2                   # This is sys.argv[2], the second argument passed
+3                   # This is sys.argv[3], the third argument passed
+

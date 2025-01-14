331e9fbcbf9e21c8927df944c0ad927e0ad340c1
neonbloo25 <theluckdemon@gmail.com>
2025-01-14 11:50:47 -0400

    New code, proper code

--- debugging/print_arguments.py
+++ debugging/print_arguments.py
@@ -1,5 +1,5 @@
-print_arguments.py  # This is sys.argv[0], the name of the script
-1                   # This is sys.argv[1], the first argument passed
-2                   # This is sys.argv[2], the second argument passed
-3                   # This is sys.argv[3], the third argument passed
+#!/usr/bin/python3
+import sys
 
+for i in range(len(sys.argv)):
+    print(sys.argv[i])
